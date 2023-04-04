35a74f6b290a9627757d41946959523baa9aa241
Nati <natnael3370@gmail.com>
2023-04-04 19:00:32 +0300

    First week challeng with typescript

---
+++ Week-01/arrayReverse.ts
@@ -0,0 +1,50 @@
+"use strict";
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function reverseArray(a: number[]): number[] {}
+
+function main() {
+  const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]);
+
+  const arrCount: number = parseInt(readLine().trim(), 10);
+
+  const arr: number[] = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const res: number[] = reverseArray(arr);
+
+  ws.write(res.join(" ") + "\n");
+
+  ws.end();
+}
