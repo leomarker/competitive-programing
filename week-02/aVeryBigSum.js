e47a7e23a69eec3b509c1276d482ce0959cb45de
leomarker <natnael3370@gmail.com>
2023-04-18 05:37:13 +0300

    aVeryBigSum with js

---
+++ week-02/aVeryBigSum.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+let ar = [1000000001 ,1000000002 ,1000000003 ,1000000004 ,1000000005]
+
+console.log(aVeryBigSum(ar));
+function aVeryBigSum(ar) {
+    let sum = 0;
+    for(let i =  0 ; i <ar.length ; i++){
+       sum +=ar[i];
+    }
+ return sum
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = aVeryBigSum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
