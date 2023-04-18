65fce04e761f9e23ab5cf281bb3ec4e93c80048c
leomarker <natnael3370@gmail.com>
2023-04-18 06:50:55 +0300

    diagonalsum challenge passed all test

---
+++ week-02/diagonalSum.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+let arr = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+];
+
+console.log(diagonalDifference(arr));
+function diagonalDifference(arr) {
+  let primaryDiagonalSum = 0;
+  let secondaryDiagonalSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (i === j) {
+        primaryDiagonalSum += arr[i][j];
+      }
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = arr[i].length; j >= arr[i].length; j--) {
+      let index = j - i - 1;
+      secondaryDiagonalSum += arr[i][index];
+    }
+  }
+  return Math.abs(primaryDiagonalSum - secondaryDiagonalSum);
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  let arr = Array(n);
+
+  for (let i = 0; i < n; i++) {
+    arr[i] = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((arrTemp) => parseInt(arrTemp, 10));
+  }
+
+  const result = diagonalDifference(arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
